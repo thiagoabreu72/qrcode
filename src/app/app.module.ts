73c0f4baabbc871b57747e89f84b13c9d13a2709
Thiago Abreu <thiagoabreu72@gmail.com>
2021-08-02 18:47:15 -0300

    Projeto QR Code - V1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { WebcamComponent } from './webcam/webcam.component';
+import { ZXingScannerModule } from '@zxing/ngx-scanner';
+import { NgQrScannerModule } from 'angular2-qrscanner';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WebcamComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    ZXingScannerModule,
+    NgQrScannerModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
